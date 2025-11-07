c154bdb8f08b6f9ffd8b6079f251856941db809b
Viktoriia Kobal <kobal.viktoriia@student.uzhnu.edu.ua>
2025-11-07 10:43:45 +0200

    feat: improvments in category card  styling

--- lib/core/widgets/categories/category_card.dart
+++ lib/core/widgets/categories/category_card.dart
@@ -1,4 +1,3 @@
-// lib/widgets/category_item.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '/../../features/doctors/domain/doctor_category.dart';
@@ -12,48 +11,99 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      borderRadius: BorderRadius.circular(16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.05),
-                  blurRadius: 10,
-                  offset: const Offset(0, 4),
-                ),
-              ],
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _HoverableIconContainer(category: category),
+            const SizedBox(height: 6),
+            _HoverableText(text: category.name),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _HoverableIconContainer extends StatefulWidget {
+  final DoctorCategory category;
+
+  const _HoverableIconContainer({required this.category});
+
+  @override
+  State<_HoverableIconContainer> createState() =>
+      _HoverableIconContainerState();
+}
+
+class _HoverableIconContainerState extends State<_HoverableIconContainer> {
+  bool _isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(_isHovered ? 0.12 : 0.05),
+              blurRadius: _isHovered ? 16 : 10,
+              offset: Offset(0, _isHovered ? 6 : 4),
             ),
-            child: SvgPicture.asset(
-              category.iconPath,
-              width: 48,
-              height: 48,
-              colorFilter: ColorFilter.mode(
-                Theme.of(context).primaryColor,
-                BlendMode.srcIn,
-              ),
-            ),
+          ],
+        ),
+        child: SvgPicture.asset(
+          widget.category.iconPath,
+          width: 48,
+          height: 48,
+          colorFilter: ColorFilter.mode(
+            Theme.of(context).primaryColor,
+            BlendMode.srcIn,
           ),
-          const SizedBox(height: 6),
-          Text(
-            category.name,
-            style: const TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w500,
-              color: Colors.grey,
-            ),
-            textAlign: TextAlign.center,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-          ),
-        ],
+        ),
+      ),
+    );
+  }
+}
+
+class _HoverableText extends StatefulWidget {
+  final String text;
+
+  const _HoverableText({required this.text});
+
+  @override
+  State<_HoverableText> createState() => _HoverableTextState();
+}
+
+class _HoverableTextState extends State<_HoverableText> {
+  bool _isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: AnimatedDefaultTextStyle(
+        duration: const Duration(milliseconds: 200),
+        style: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w500,
+          color: _isHovered ? Colors.grey[700] : Colors.grey,
+        ),
+        child: Text(
+          widget.text,
+          textAlign: TextAlign.center,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
       ),
     );
   }
