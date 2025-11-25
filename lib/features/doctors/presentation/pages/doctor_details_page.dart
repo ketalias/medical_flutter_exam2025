420739900ec0e60a228e62be79efd86a2b244fff
Viktoriia Kobal <kobal.viktoriia@student.uzhnu.edu.ua>
2025-11-25 22:22:41 +0200

    Merge branch feature/doctor_page into dev"

--- lib/features/doctors/presentation/pages/doctor_details_page.dart
+++ lib/features/doctors/presentation/pages/doctor_details_page.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 import '../../../../data/doctor_repository.dart';
 import '../../../../models/doctor_model.dart';
-import '../../../../widgets/doctor_card.dart';
+import 'package:medical_flutter_exam2025/features/doctors/widgets/doctor_card.dart';
 
 class DoctorDetailsPage extends StatefulWidget {
-  final String doctorId;
-  const DoctorDetailsPage({super.key, required this.doctorId});
+  final DoctorModel doctor;
+  const DoctorDetailsPage({super.key, required this.doctor});
 
   @override
   State<DoctorDetailsPage> createState() => _DoctorDetailsPageState();
 }
 
 class _DoctorDetailsPageState extends State<DoctorDetailsPage> {
-  final DoctorRepository _repo = DoctorRepository();
-  DoctorModel? doctor;
-
+  late DoctorModel doctor;
   int selectedDate = -1;
   int selectedTime = -1;
 
@@ -38,20 +36,10 @@ class _DoctorDetailsPageState extends State<DoctorDetailsPage> {
     "19:00",
   ];
 
-  bool isLoading = true;
-
   @override
   void initState() {
     super.initState();
-    _loadDoctor();
-  }
-
-  Future<void> _loadDoctor() async {
-    final doc = await _repo.getDoctorById(widget.doctorId);
-    setState(() {
-      doctor = doc;
-      isLoading = false;
-    });
+    doctor = widget.doctor;
   }
 
   @override
@@ -72,204 +60,176 @@ class _DoctorDetailsPageState extends State<DoctorDetailsPage> {
         elevation: 0,
         centerTitle: true,
       ),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 4),
-                  DoctorCard(doctor: doctor!),
-                  const SizedBox(height: 16),
-                  const Text(
-                    "Про лікаря",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(height: 10),
-
-                  RichText(
-                    text: TextSpan(
-                      style: const TextStyle(
-                        fontSize: 14,
-                        color: Colors.grey,
-                        height: 1.55,
-                      ),
-                      children: [
-                        const TextSpan(
-                          text:
-                              "Лікар з понад 12-річним досвідом роботи. Спеціалізується на профілактиці, "
-                              "ранній діагностиці та лікуванні дитячих захворювань. Проводить консультації...",
-                        ),
-                        const TextSpan(
-                          text: "Читати далі",
-                          style: TextStyle(
-                            color: Color(0xFF00897B),
-                            fontWeight: FontWeight.w600,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 4),
+            DoctorCard(doctor: doctor),
+            const SizedBox(height: 16),
+            const Text(
+              "Про лікаря",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Лікар з багаторічним досвідом роботи. Спеціалізується на консультаціях та лікуванні. Уважно ставиться до пацієнтів та підбирає індивідуальні методи лікування.",
+              style: const TextStyle(fontSize: 14, color: Colors.grey),
+            ),
+            const SizedBox(height: 15),
+            // Дати
+            Center(
+              child: SizedBox(
+                height: 78,
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: dates.length,
+                  separatorBuilder: (_, __) => const SizedBox(width: 14),
+                  itemBuilder: (context, index) {
+                    final isSelected = selectedDate == index;
+                    return GestureDetector(
+                      onTap: () => setState(() => selectedDate = index),
+                      child: Container(
+                        width: 62,
+                        decoration: BoxDecoration(
+                          color: isSelected
+                              ? const Color(0xFF00897B)
+                              : Colors.white,
+                          borderRadius: BorderRadius.circular(18),
+                          border: Border.all(
+                            color: isSelected
+                                ? const Color(0xFF00897B)
+                                : Colors.grey.shade300,
+                            width: isSelected ? 0 : 1.5,
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-
-                  const SizedBox(height: 24),
-
-                  Center(
-                    child: SizedBox(
-                      height: 78,
-                      child: ListView.separated(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        itemCount: dates.length,
-                        separatorBuilder: (_, __) => const SizedBox(width: 14),
-                        itemBuilder: (context, index) {
-                          final isSelected = selectedDate == index;
-                          return GestureDetector(
-                            onTap: () => setState(() => selectedDate = index),
-                            child: Container(
-                              width: 62,
-                              decoration: BoxDecoration(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              dates[index]["weekday"]!,
+                              style: TextStyle(
+                                fontSize: 13,
                                 color: isSelected
-                                    ? const Color(0xFF00897B)
-                                    : Colors.white,
-                                borderRadius: BorderRadius.circular(18),
-                                border: Border.all(
-                                  color: isSelected
-                                      ? const Color(0xFF00897B)
-                                      : Colors.grey.shade300,
-                                  width: isSelected ? 0 : 1.5,
-                                ),
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    dates[index]["weekday"]!,
-                                    style: TextStyle(
-                                      fontSize: 13,
-                                      color: isSelected
-                                          ? Colors.white70
-                                          : Colors.grey.shade600,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 6),
-                                  Text(
-                                    dates[index]["day"]!,
-                                    style: TextStyle(
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.bold,
-                                      color: isSelected
-                                          ? Colors.white
-                                          : Colors.black87,
-                                    ),
-                                  ),
-                                ],
+                                    ? Colors.white70
+                                    : Colors.grey.shade600,
+                                fontWeight: FontWeight.w500,
                               ),
                             ),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-
-                  const SizedBox(height: 20),
-                  Container(height: 0.8, color: Colors.grey.shade300),
-                  const SizedBox(height: 20),
-
-                  Expanded(
-                    child: GridView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      padding: EdgeInsets.zero,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3,
-                            childAspectRatio: 3.2,
-                            mainAxisSpacing: 14,
-                            crossAxisSpacing: 14,
-                          ),
-                      itemCount: times.length,
-                      itemBuilder: (context, index) {
-                        final isSelected = selectedTime == index;
-                        return GestureDetector(
-                          onTap: () => setState(() => selectedTime = index),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: isSelected
-                                  ? const Color(0xFF00897B)
-                                  : Colors.white,
-                              borderRadius: BorderRadius.circular(30),
-                              border: Border.all(
+                            const SizedBox(height: 6),
+                            Text(
+                              dates[index]["day"]!,
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
                                 color: isSelected
-                                    ? const Color(0xFF00897B)
-                                    : Colors.grey.shade400,
-                                width: 1,
+                                    ? Colors.white
+                                    : Colors.black87,
                               ),
                             ),
-                            child: Center(
-                              child: Text(
-                                times[index],
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w600,
-                                  color: isSelected
-                                      ? Colors.white
-                                      : Colors.black87,
-                                ),
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-
-                  const SizedBox(height: 24),
-
-                  SizedBox(
-                    width: double.infinity,
-                    height: 56,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (selectedDate == -1 || selectedTime == -1) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text("Оберіть дату та час"),
-                            ),
-                          );
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              backgroundColor: Colors.teal.shade600,
-                              content: const Text(
-                                "Ви успішно записані!",
-                                style: TextStyle(fontWeight: FontWeight.w600),
-                              ),
-                            ),
-                          );
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF00897B),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        elevation: 0,
-                      ),
-                      child: const Text(
-                        "Записатися на консультацію",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white,
+                          ],
                         ),
                       ),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                ],
+                    );
+                  },
+                ),
               ),
             ),
+
+            const SizedBox(height: 20),
+            Container(height: 0.8, color: Colors.grey.shade300),
+            const SizedBox(height: 20),
+
+            // Часи
+            Expanded(
+              child: GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                padding: EdgeInsets.zero,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 3.2,
+                  mainAxisSpacing: 14,
+                  crossAxisSpacing: 14,
+                ),
+                itemCount: times.length,
+                itemBuilder: (context, index) {
+                  final isSelected = selectedTime == index;
+                  return GestureDetector(
+                    onTap: () => setState(() => selectedTime = index),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: isSelected
+                            ? const Color(0xFF00897B)
+                            : Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(
+                          color: isSelected
+                              ? const Color(0xFF00897B)
+                              : Colors.grey.shade400,
+                          width: 1,
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          times[index],
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            color: isSelected ? Colors.white : Colors.black87,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            const SizedBox(height: 24),
+            SizedBox(
+              width: double.infinity,
+              height: 56,
+              child: ElevatedButton(
+                onPressed: () {
+                  if (selectedDate == -1 || selectedTime == -1) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Оберіть дату та час")),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        backgroundColor: Colors.teal.shade600,
+                        content: const Text(
+                          "Ви успішно записані!",
+                          style: TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF00897B),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  elevation: 0,
+                ),
+                child: const Text(
+                  "Записатися на консультацію",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/features/doctors/widgets/doctor_card.dart
+++ lib/features/doctors/widgets/doctor_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../models/doctor_model.dart';
+import 'package:medical_flutter_exam2025/features/doctors/presentation/pages/doctor_details_page.dart';
 
 class DoctorCard extends StatelessWidget {
   final DoctorModel doctor;
@@ -21,89 +22,97 @@ class DoctorCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      color: const Color.fromARGB(255, 255, 255, 255),
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Padding(
-        padding: const EdgeInsets.all(6.0),
-        child: Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Container(
-                width: 128,
-                height: 128,
-                child: doctor.imageUrl.isNotEmpty
-                    ? Image.network(
-                        doctor.imageUrl,
-                        fit: BoxFit.cover,
-                        errorBuilder: (context, error, stackTrace) =>
-                            _buildInitials(),
-                      )
-                    : _buildInitials(),
-              ),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => DoctorDetailsPage(doctor: doctor)),
+        );
+      },
 
-            const SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    doctor.fullName,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
+      child: Card(
+        elevation: 2,
+        color: const Color.fromARGB(255, 255, 255, 255),
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  width: 128,
+                  height: 128,
+                  child: doctor.imageUrl.isNotEmpty
+                      ? Image.network(
+                          doctor.imageUrl,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) =>
+                              _buildInitials(),
+                        )
+                      : _buildInitials(),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      doctor.fullName,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                      ),
+                      overflow: TextOverflow.ellipsis,
                     ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    doctor.specialty,
-                    style: TextStyle(color: Colors.grey[700], fontSize: 14),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  const SizedBox(height: 8),
-                  Container(
-                    width: 68,
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(17, 12, 206, 196),
-                      borderRadius: BorderRadius.circular(8),
+                    const SizedBox(height: 4),
+                    Text(
+                      doctor.specialty,
+                      style: TextStyle(color: Colors.grey[700], fontSize: 14),
+                      overflow: TextOverflow.ellipsis,
                     ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 6,
-                      vertical: 4,
-                    ),
-                    child: Row(
-                      children: [
-                        const Icon(
-                          Icons.thumb_up,
-                          size: 16,
-                          color: Color.fromARGB(255, 37, 145, 153),
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          '${doctor.likes}',
-                          style: const TextStyle(
-                            fontSize: 13,
+                    const SizedBox(height: 8),
+                    Container(
+                      width: 68,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(17, 12, 206, 196),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 6,
+                        vertical: 4,
+                      ),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.thumb_up,
+                            size: 16,
                             color: Color.fromARGB(255, 37, 145, 153),
                           ),
-                        ),
-                      ],
+                          const SizedBox(width: 4),
+                          Text(
+                            '${doctor.likes}',
+                            style: const TextStyle(
+                              fontSize: 13,
+                              color: Color.fromARGB(255, 37, 145, 153),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    doctor.location ?? '',
-                    style: TextStyle(color: Colors.grey[700], fontSize: 12),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
+                    const SizedBox(height: 4),
+                    Text(
+                      doctor.location ?? '',
+                      style: TextStyle(color: Colors.grey[700], fontSize: 12),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
